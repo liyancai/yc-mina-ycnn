8ac3c04106c1d44d75655c11aad2fd558e1f5725
liyancai <liyancai1986@163.com>
2018-08-20 15:10:58 +0800

    封装绘制圆角矩形边框方法

--- pages/demo/canvas.js
+++ pages/demo/canvas.js
@@ -1,21 +1,20 @@
 const canvasUtil = require('../../utils/canvas.js')
 
 Page({
-  data: {
+    data: {
   
-  },
-  onLoad: function (options) {
-      var that = this
-      var ctx = wx.createCanvasContext('radiusRect')
+    },
+    onLoad: function (options) {
+        var that = this
+        var ctx = wx.createCanvasContext('radiusRect')
 
-      ctx.setFillStyle('skyblue')
+        ctx.setFillStyle('skyblue')
+        canvasUtil.fillRadiusRect(ctx, 30, 30, 300, 200, 10)
 
+        ctx.setStrokeStyle('red')
+        canvasUtil.strokeRadiusRect(ctx, 30, 30, 300, 200, 10)
 
-
-
-      canvasUtil.fillRadiusRect(ctx, 30, 30, 300, 200, 10)
-
-      ctx.draw()
-  },
+        ctx.draw()
+    },
 
 })
--- utils/canvas.js
+++ utils/canvas.js
@@ -60,7 +60,30 @@ const fillRadiusRect = (ctx, x, y, w, h, r) => {
     ctx.fill();
 }
 
+/**
+ * ctx : canvas上下文对象
+ * x: 起始位置横轴坐标
+ * y: 起始位置纵轴坐标
+ * w: 矩形宽度
+ * h: 矩形高度
+ * r: 圆角大小
+ */
+const strokeRadiusRect = (ctx, x, y, w, h, r) => {
+
+    ctx.beginPath();
+
+    ctx.moveTo(x + r, y);
+
+    ctx.arcTo(x + w, y, x + w, y + r, r);
+    ctx.arcTo(x + w, y + h, x + w - r, y + h, r);
+    ctx.arcTo(x, y + h, x, y + h - r, r);
+    ctx.arcTo(x, y, x + r, y, r);
+
+    ctx.stroke();
+}
+
 module.exports = {
     fillMultipleText: fillMultipleText,
-    fillRadiusRect: fillRadiusRect
+    fillRadiusRect: fillRadiusRect,
+    strokeRadiusRect: strokeRadiusRect
 }
