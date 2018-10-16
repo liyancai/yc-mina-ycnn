2e6d54e69dcbb759cc71c6987938edfe9e58e502
liyancai <liyancai1986@163.com>
2018-10-16 10:37:45 +0800

    @

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 //app.js
 App({
     onLaunch: function () {
-
+      if (!wx.cloud) {
+        console.error('请使用 2.2.3 或以上的基础库以使用云能力')
+      } else {
+        wx.cloud.init({
+          traceUser: true,
+        })
+      }
     },
     showToast: function (title) {
         wx.showToast({ title: title, icon: 'none' })
